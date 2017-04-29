193d8c8504fa631aff781d6fe13d2e522671f692
Tanaka Hayate <pinqphayate@gmail.com>
2017-04-30 08:16:33 +0900

    add module to scrape race id from source code

--- src/page_scraping3.py
+++ src/page_scraping3.py
@@ -6,6 +6,8 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import urllib.request
 
+DOMAIN = 'http://db.netkeiba.com/'
+
 
 def scrape_race_info(url, output_file):
     # read page source code
@@ -149,16 +151,17 @@ def scrape_horse_history(hid):
     # return history_df.T    # return list
 
 
-def scrape_rid():
+def scrape_rid(word, source):
     '''
     1. read page source
     2. scrape rid (race id)
     return -> race_id list
+    // TODO: 1単語だけの検索だと、他のレースが出てきてしまう場合がある
     '''
     # source = './../Resource/stayers'    # must get this page source by hand
     # must get this page source by hand
-    source = './../Resource/race-history.html'
-    soup = BeautifulSoup(open(source, encoding='eucjp'), "lxml")
+    #
+    soup = BeautifulSoup(source, "lxml")
     table = soup.find("table", attrs={"class": "nk_tb_common race_table_01"})
     list = []
     # limitter for 10 years
@@ -167,77 +170,60 @@ def scrape_rid():
             break
 
         for td in tr.findAll("td", attrs={"class": "txt_l"}):
-            # links = td.find_all('a')
             for link in td.findAll('a'):
-                # if 'href' in link.attrs:
                 url = link.attrs['href']
                 title = link.attrs['title']
-                if "race" in url and '高松宮記念' in title:
+                if "race" in url and word in title:
                     tmp = url.split('/')
-                    list.append(tmp[4])
+                    list.append(tmp[2])
 
-    with open('./../Resource/rid_list.csv', 'w') as f:
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerow(list)
     return list
+
 
 def get_request_via_post(word):
     url = 'http://db.netkeiba.com/'
     # create paramator with consts x=0,y=0,pid=race_list
     _param = _param_creator(word=word)
-    with urllib.request.urlopen(url, data=_param.encode('utf8')) as res:
+    with urllib.request.urlopen(DOMAIN, data=_param.encode('utf8')) as res:
         html = res.read().decode('euc-jp', 'ignore')
     return html
 
 
 def _param_creator(word):
     prefix = 'y=0&word='
-    keyword = urllib.parse.quote_plus(word,encoding="eucjp")
+    keyword = urllib.parse.quote_plus(word, encoding="eucjp")
     suffix = '&pid=race_list&x=0'
     return prefix + keyword + suffix
 
 
-def scrape_race_id(source):
-    soup = BeautifulSoup(source, "lxml")
-    table = soup.find(class_='nk_tb_common race_table_01')
-    for tr in table.findAll('tr', ''):
-        list = []
-        for td in tr.findAll('td', ''):
-            # get house status
-            word = " ".join(td.text.rsplit())
-            list.append(word.encode('utf-8'))
-    print(list)
-
-
-
 def main():
-    # get race_id from source on locdal directory
     # 1. get supecified race ids
     word = '皐月賞'
     source = get_request_via_post(word)
-    rid_list = scrape_race_id(source)
+    rids = scrape_rid(word, source)
+    print(rids)
 
-    # rids = scrape_rid()
-    # hid_list = []
-    #
-    # for year in rids:
-    #     url = 'http://db.netkeiba.com/race/' + year + '/'
-    #     output_file = year + '.csv'
-    #     # scrape RACE data
-    #     print('get list of hource_id: ' + year)
-    #     # get list of hource_id who attend the race in year(rid)
-    #     old_rids = scrape_race_info(url, output_file)
-    #
-    #     print('get history of house')
-    #     for old_rid in old_rids:
-    #         # get history of house which attend the race in year(rid)
-    #         scrape_horse_history(old_rid)
-    #
-    #     # scrape RATE data
-    #     scrape_res(url, output_file)
-    #
-    # # normalize rate data
-    # scrape_race_odds(rids)
+    hid_list = []
+
+    for year in rids:
+        url = DOMAIN　+'race/' + year + '/'
+        output_file = year + '.csv'
+        # scrape RACE data
+        print('get list of hource_id: ' + year)
+        # get list of hource_id who attend the race in year(rid)
+        old_rids = scrape_race_info(url, output_file)
+
+        print('get history of house')
+        for old_rid in old_rids:
+            # get history of house which attend the race in year(rid)
+            scrape_horse_history(old_rid)
+
+        # scrape RATE data
+        scrape_res(url, output_file)
+
+    # normalize rate data
+    scrape_race_odds(rids)
+
 
 if __name__ == '__main__':
     main()
