9d34259d92228b0bf249aa2215783397d127d6bd
PinkPhayate <pinqphayate@gmail.com>
2017-06-25 17:25:00 +0900

    [WIP]今日のレースIDを取得するモジュールを作成

--- src/today_scraping3.py
+++ src/today_scraping3.py
@@ -38,14 +38,31 @@ class OddsMan(object):
             df.append(row)
         return df
 
+    def scrape_race_id(self, source):
+        soup = BeautifulSoup(source, "lxml")
+        df = []
+        body = soup.find(id="race_list_body")
+        for col in body.findAll(class_='race_top_hold_list'):
+            for div in col.findAll(class_='racename'):
+                print(div.text)
+
     def get_race_odds(self, race_id):
         url = 'http://race.netkeiba.com/?pid=race_old&id=c' + str(race_id)
-        self.source = self.get_request_via_get(url)
-        self.df = self.scrape_race_info(self.source)
+        source = self.get_request_via_get(url)
+        self.df = self.scrape_race_info(source)
         odds_list = [x[10] for x in self.df if len(x) > 0]
         return odds_list
 
+    def get_race_ids(self, date):
+        url = 'http://race.netkeiba.com/?pid=race_list&id=c' + str(date)
+        source = self.get_request_via_get(url)
+        self.df = self.scrape_race_id(source)
+
+
+
+
 # race_id = '201702010401'
-# odds_man = OddsMan()
+odds_man = OddsMan()
 # odds_dict = odds_man.get_race_odds(race_id)
+odds_dict = odds_man.get_race_ids('0625')
 # print(odds_dict)
